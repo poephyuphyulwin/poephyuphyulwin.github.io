3a4bafb7c90180853b9678c8d4c746567da1acba
Poe Phyu Phyu Lwin <32502935+poephyuphyulwin@users.noreply.github.com>
2020-07-03 16:45:35 +0630

    Add files via upload

---
+++ js/material.js
@@ -0,0 +1,12 @@
+function over(e){
+	document.getElementById('portal').innerHTML = e.alt;
+}
+
+function leave(){
+	document.getElementById('portal').innerHTML='Poe Phyu Phyu Lwin\'s Portal';
+}
+
+function changeColor(){
+	c = document.getElementById('mail');
+	c.style.color='green';
+} 
